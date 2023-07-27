61e76e3bae97692cc608d30f7a3a4e0b2c4ed37f
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-07-26 22:39:44 -0300

    Add markdown rendering to PostList

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -3,32 +3,40 @@ import { formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import { Post, Body, ContainerPost, CardLimit, InfosPost } from "./style";
 import { ContextPost } from "../../contexts/ContextPost";
+import ReactMarkdown from "react-markdown";
 
 export function PostsList() {
   const { postData } = useContext(ContextPost);
+  const maxLength = 200;
   return (
     <CardLimit>
       <ContainerPost>
-        {postData.map((post) => (
-          <Post key={post.number}>
-            <div>
-              <InfosPost>
-                <h2>{post.title}</h2>
-                <p>
-                  {formatDistanceToNow(new Date(post.created_at), {
-                    addSuffix: true,
-                    locale: ptBR,
-                  })}
-                </p>
-              </InfosPost>
-              <Body>
-                {post.body.length > 250
-                  ? post.body.substring(0, 250) + "..."
-                  : post.body}
-              </Body>
-            </div>
-          </Post>
-        ))}
+        {postData.map((post) => {
+          let markdown = post.body;
+
+          if (markdown.length > maxLength) {
+            markdown = markdown.substring(0, maxLength) + "...";
+          }
+
+          return (
+            <Post key={post.number}>
+              <div>
+                <InfosPost>
+                  <h2>{post.title}</h2>
+                  <p>
+                    {formatDistanceToNow(new Date(post.created_at), {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </p>
+                </InfosPost>
+                <Body>
+                  <ReactMarkdown children={markdown} />
+                </Body>
+              </div>
+            </Post>
+          );
+        })}
       </ContainerPost>
     </CardLimit>
   );
